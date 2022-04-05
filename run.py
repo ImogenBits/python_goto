3d82fe2f69e8f3246d62bc170a293c3b4d8f84ac
Imogen <me@imogen.tech>
2022-04-05 16:17:10 +0200

    fggh

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
 "test"
 
+import control_flow.blep
+
 print(1)
--- setup.py
+++ setup.py
@@ -1,8 +1,27 @@
 from setuptools import setup
+from setuptools.command.build_py import build_py
+from setuptools.command.develop import develop
+from os.path import join, dirname
+from os import symlink, remove
+
+class build_with_pth(build_py):
+    def run(self):
+        super().run()
+        out = join(self.build_lib, "control_flow.pth")
+        self.copy_file("control_flow.pth", out, preserve_mode=0)
+
+class develop_with_pth(develop):
+    def run(self):
+        if self.uninstall:
+            remove(join(self.install_dir, "control_flow.pth"))
+        super().run()
+        if not self.uninstall:
+            out = join(self.install_dir, "control_flow.pth")
+            symlink(join(dirname(__file__), "control_flow.pth"), out)
 
 setup(
     name = "control_flow",
     packages = ["control_flow"],
     python_requires = ">=3.7",
-    package_dir={"": "src"}
+    cmdclass={"build_py": build_with_pth, "develop": develop_with_pth},
 )
